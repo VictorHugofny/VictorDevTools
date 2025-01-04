93ddc4f6f7de157141b499e52421918ce522b9f2
Victor Hugo Santana <victorhugofny@gmail.com>
2025-01-03 23:51:23 -0300

    ajuste rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,16 +3,16 @@ import Navbar from './componentes/Navbar';
 import ConversorCSV from './views/conversorCSV';
 import GerarSenhas from './views/geradorSenhas';
 
-function RoutesAPP(){
-    return(
-        <Router>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<ConversorCSV/>}/>
-                <Route path="/senhas" element={<GerarSenhas/>}/>
-            </Routes>
-        </Router>
-    )
+function RoutesAPP() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ConversorCSV />} />
+        <Route path="/senhas" element={<GerarSenhas />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default RoutesAPP;
