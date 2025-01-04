6a3593cf961e6ad06260911e477e87bd621baddf
Victor Hugo Santana <victorhugofny@gmail.com>
2025-01-03 23:48:14 -0300

    ajustes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './componentes/Navbar';
 import ConversorCSV from './views/conversorCSV';
 import GerarSenhas from './views/geradorSenhas';
 
 function RoutesAPP(){
     return(
-        <BrowserRouter>
+        <Router>
             <Navbar/>
             <Routes>
                 <Route path="/" element={<ConversorCSV/>}/>
                 <Route path="/senhas" element={<GerarSenhas/>}/>
             </Routes>
-        </BrowserRouter>
+        </Router>
     )
 }
 
